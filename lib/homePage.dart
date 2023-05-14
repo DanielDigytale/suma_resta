35ef0f81e0f75a2932b1016590a99675fb737826
DanielDigytale <daniel.fernandez@digytale.net>
2023-05-15 01:14:50 +0200

    HomePage completed but Navigation needed

---
+++ lib/homePage.dart
@@ -0,0 +1,190 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String _operation = '';
+  int _operationHeight = 0;
+  MaterialColor buttonColor = Colors.yellow;
+
+  void _setOperationAdd(){
+    setState(() {
+      _operation = '+';
+    });
+  }
+  void _setOperationSubtract(){
+    setState(() {
+      _operation = '-';
+    });
+  }
+
+  void _setOperationHeight({required int number}){
+    setState(() {
+      _operationHeight = number;
+    });
+  }
+
+  void _setButtonColor(){
+    setState(() {
+      if (buttonColor==Colors.yellow) {
+        buttonColor = Colors.deepOrange;
+      } else {
+        buttonColor = Colors.yellow;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text(widget.title)),
+      ),
+      body: Container(
+        color: Colors.black54,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Container(
+                height: 300,
+                width: 300,
+                color: Colors.blueGrey,
+                child:Center(
+                  child: Column(
+                    children: [
+                      Text(
+                          'Elige la operación:',
+                          style: TextStyle(fontSize: 30, color: Colors.white),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          TextButton(
+                              onPressed: _setOperationAdd,
+                              child: Text(
+                                  '+',
+                                style: TextStyle(
+                                  fontSize: 100.0,
+                                  color: (_operation=='+')
+                                      ? Colors.deepOrange
+                                      : buttonColor,
+                                ),
+                              ),
+                          ),
+                          TextButton(
+                            onPressed: _setOperationSubtract,
+                            child: Text(
+                              '-',
+                              style: TextStyle(
+                                fontSize: 100.0,
+                                color: (_operation=='-')
+                                    ? Colors.deepOrange
+                                    : buttonColor,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                height: 300,
+                width: 300,
+                color: Colors.blueGrey[700],
+                child:Center(
+                  child: Column(
+                    children: [
+                      Text(
+                          'Elige el tamaño de los números:',
+                        style: TextStyle(fontSize: 30, color: Colors.white),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          TextButton(
+                            onPressed: () => _setOperationHeight(number: 2),
+                            child: Text(
+                              '2',
+                              style: TextStyle(
+                                fontSize: 100.0,
+                                color: (_operationHeight==2)
+                                  ? Colors.deepOrange
+                                  : buttonColor
+                              ),
+                            ),
+                          ),
+                          TextButton(
+                            onPressed: () => _setOperationHeight(number: 3),
+                            child: Text(
+                              '3',
+                              style: TextStyle(
+                                fontSize: 100.0,
+                                color: (_operationHeight==3)
+                                    ? Colors.deepOrange
+                                    : buttonColor,
+                              ),
+                            ),
+                          ),
+                          TextButton(
+                            onPressed: () => _setOperationHeight(number: 4),
+                            child: Text(
+                              '4',
+                              style: TextStyle(
+                                fontSize: 100.0,
+                                color: (_operationHeight==4)
+                                    ? Colors.deepOrange
+                                    : buttonColor,
+                              ),
+                            ),
+                          ),
+                          TextButton(
+                            onPressed: () => _setOperationHeight(number: 5),
+                            child: Text(
+                              '5',
+                              style: TextStyle(
+                                fontSize: 100.0,
+                                color: (_operationHeight==5)
+                                    ? Colors.deepOrange
+                                    : buttonColor,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              TextButton(
+                onPressed: _setButtonColor,
+                style: ButtonStyle(
+                    backgroundColor: MaterialStatePropertyAll(Colors.blue),
+                ),
+                child: Text(
+                  'continuar',
+                  style: TextStyle(
+                    fontSize: 60.0,
+                    color: buttonColor,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+
+    );
+  }
+}
+
+
